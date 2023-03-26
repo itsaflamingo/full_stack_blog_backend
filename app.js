e453f037412f21f6ff9d31bafa6c7df34a760dc6
Liz <elizabeth.pinero.r@gmail.com>
2023-03-26 01:10:58 -0400

    Changed order of app.js middleware

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ dotenv.config();
 const app = express();
 
 // Enable CORS from all domains for all requests
+app.use(cors());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  console.log('Headers:', req.headers);
   next();
 });
 
