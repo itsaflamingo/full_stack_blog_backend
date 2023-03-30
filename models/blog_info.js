6bfee978d9747b3cc9ea7885a39c0f4f5372d7cd
Liz <elizabeth.pinero.r@gmail.com>
2023-03-30 14:30:33 -0400

    Installed he package to decode image URLs

--- models/blog_info.js
+++ models/blog_info.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const he = require('he');
 
 const Schema = mongoose.Schema;
 
@@ -6,6 +7,17 @@ const BlogInfoSchema = new Schema({
     picture:      { type: String, required: true },
     description:  { type: String, required: true },
 });
+
+// Middleware to decode the picture URL before saving the document
+BlogInfoSchema.pre('save', function (next) {
+    // Check if the picture field has been modified
+    if (this.isModified('picture')) {
+      // Decode the URL using the he library
+      this.picture = he.decode(this.picture);
+    }
+    // Call the next middleware
+    next();
+  });
 
 // Add virtual. Use function() to access 'this'.
 BlogInfoSchema.virtual('url').get(function() {
