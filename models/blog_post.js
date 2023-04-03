29214812fdc93de3f93d2c17614b8cd9e16f33b8
Liz <elizabeth.pinero.r@gmail.com>
2023-04-03 18:14:18 -0400

    Changed router endpoint from post to delete

--- models/blog_post.js
+++ models/blog_post.js
@@ -4,8 +4,8 @@ const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
-    title:      { type: String, required: true },
-    body:       { type: String, required: true },
+    title:      { type: String,  required: true },
+    body:       { type: String,  required: true },
     published:  { type: Boolean, required: true },
     date:       { type: Date, default: Date.now, required: true },
 }, {
--- routes/blog_secure.js
+++ routes/blog_secure.js
@@ -7,15 +7,15 @@ const blog_controller      = require('../controllers/blogController'    );
 const comment_controller   = require('../controllers/commentController' );
 const blog_info_controller = require('../controllers/blogInfoController');
 
-router.get  ('/',                     blog_controller.all_posts_list       );
-router.get  ('/post/:id',             blog_controller.single_post_all      );
-router.post ('/post/create',          blog_post_controller.create_post     );
-router.get  ('/post/:id/update',      blog_post_controller.update_get      );
-router.patch('/post/:id/update',      blog_post_controller.update_post     );
-router.post ('/post/:id/delete',      blog_post_controller.delete_post     );
-router.post ('/post/:id/:cid/delete', comment_controller.delete_comment    );
-router.post ('/description',          blog_info_controller.create_blog_info);
-router.get  ('/description',          blog_info_controller.blog_info       );
-router.patch('/description/:id',      blog_info_controller.edit_blog_info  );
+router.get    ('/',                     blog_controller.all_posts_list       );
+router.get    ('/post/:id',             blog_controller.single_post_all      );
+router.post   ('/post/create',          blog_post_controller.create_post     );
+router.get    ('/post/:id/update',      blog_post_controller.update_get      );
+router.patch  ('/post/:id/update',      blog_post_controller.update_post     );
+router.delete ('/post/:id/delete',      blog_post_controller.delete_post     );
+router.delete ('/post/:id/:cid/delete', comment_controller.delete_comment    );
+router.post   ('/description',          blog_info_controller.create_blog_info);
+router.get    ('/description',          blog_info_controller.blog_info       );
+router.patch  ('/description/:id',      blog_info_controller.edit_blog_info  );
 
 module.exports = router;
